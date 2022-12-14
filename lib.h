2716797e3c1dc5645444d5cc4542de19787998d5
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-12-14 23:25:37 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,7 +1,4 @@
-#IFNDEF _GITHUB_LIB_H
-#Definire _GITHUB_LIB_H
-void riempi(char [10][20]);
-void riempi2(char[20]);
-bool conf(char[10][20], char [10]);
-void analisi(char a[10][20], char b[20]);
-#endif //_GITHUB_LIB_H
+#ifndef ESERCITAZIONE9_2_LIB_H
+#Definire ESERCITAZIONE9_2_LIB_H
+bool comp(char [10][20], int &, char []);
+#endif //ESERCITAZIONE9_2_LIB_H
