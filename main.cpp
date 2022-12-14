54f2a2c53a58b6dd625ad942fcff3be04b6dc5e0
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-12-14 23:33:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,26 +2,25 @@
 #Includi "lib.h"
 Utilizzo dello spazio dei nomi STD;
 
+
 int main() {
-    Elenco char[10][20];
-    Nome del carattere[20];
-    int p = 0;
 
-    for(int i=0;i<10;i++){
-        for(int j=0;j<20;j++) {
- nome[j] = ' ';
-            list[i][j] = ' ';
-        }
-    }
-    for(int i=0;i<10;i++){
- cin >> lista[i];
+    char elenco [10][20];
+    char nome [20];
+
+    for (int i=0; i<10; i++) {
+        cin >> elenco[i];
     }
 
- cin >> nome;
-    if(comp(list;p,name)){
-        cout << p;
-    }altro{
+    cin >> nome;
+
+    int a = funzione(elenco, nome);
+    if (a <= 10) {
+        cout << a;
+    }
+    altro {
  cout << "non presente";
     }
+
     ritorno 0;
 }
