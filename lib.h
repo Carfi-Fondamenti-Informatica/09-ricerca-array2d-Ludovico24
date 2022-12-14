5a932fe8a629651dd92767e8f66e9b8ffec7b3f2
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-12-14 23:31:55 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,4 +1,1 @@
-#ifndef ESERCITAZIONE9_2_LIB_H
-#Definire ESERCITAZIONE9_2_LIB_H
-bool comp(char [10][20], int &, char []);
-#endif //ESERCITAZIONE9_2_LIB_H
+int funzione (char elenco [][20], char nome  [20]);
