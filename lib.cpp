6fed60979a4ffcf0d893a06a8368f5ff2ba8b1d7
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-12-14 23:30:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,12 +1,13 @@
-int bodyguard(char lista[10][20], char nome[20]){
-	  int i = 0;
-	  int j = 0;
-	  mentre ((j < 19) e (i < 10)) {
-	    if (lista[i][j] != nome[j]) {
-	      i++;
-	      j = 0;
-	    } else if (lista[i][j] == nome[j]) {
-	 J++;
-	    }
-	  } ritorno i;
-	}
+#Includi "lib.h"
+#Includi "cstring"
+
+int funzione (char elenco [10][20], char nome[20]) {
+
+    for (int i = 0; i < 10; i++) {
+        if (strcmp(elenco [i], nome) == 0) {
+            ritorno i;
+        }
+    }
+
+    ritorno 44;
+}
