00ad5dbe34af74fb9f0c66c32d36e88b8968308d
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-12-14 23:23:13 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,7 @@
-int bodyguard(char[10][20], char[20]);
+#IFNDEF _GITHUB_LIB_H
+#Definire _GITHUB_LIB_H
+void riempi(char [10][20]);
+void riempi2(char[20]);
+bool conf(char[10][20], char [10]);
+void analisi(char a[10][20], char b[20]);
+#endif //_GITHUB_LIB_H
