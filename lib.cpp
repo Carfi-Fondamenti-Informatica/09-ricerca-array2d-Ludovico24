231ac05401779e1aa4b21085528e293928092ecc
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-12-14 23:17:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+int bodyguard(char lista[10][20], char nome[20]){
+  int i = 0;
+  int j = 0;
+  mentre ((j < 19) e (i < 10)) {
+    if (lista[i][j] != nome[j]) {
+      i++;
+      j = 0;
+    } else if (lista[i][j] == nome[j]) {
+ J++;
+    }
+  } ritorno i;
+}
